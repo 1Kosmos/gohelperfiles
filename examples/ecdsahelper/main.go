80327f5fa8084697bdff7e4f28a15bb27a977f2f
ldapster <mike.dugan@1kosmos.com>
2024-02-12 08:51:31 -0600

    PL20-7081 Create BIDECDSA.go

--- examples/ecdsahelper/main.go
+++ examples/ecdsahelper/main.go
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2018, 1Kosmos Inc. All rights reserved.
+ * Licensed under 1Kosmos Open Source Public License version 1.0 (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of this license at
+ *    https://github.com/1Kosmos/1Kosmos_License/blob/main/LICENSE.txt
+ */
+
 package main
 
 import (
@@ -5,7 +13,7 @@ import (
 	"fmt"
 	"os"
 
-	BIDECDSA "github.com/1Kosmos/gohelperfiles"
+	"github.com/1Kosmos/gohelperfiles"
 )
 
 const CLEAR_TEXT = "Clear text to encrypt."
@@ -14,26 +22,26 @@ func main() {
 	var clearText string
 	flag.StringVar(&clearText, "text", CLEAR_TEXT, "clear text to test")
 	flag.Parse()
-	localPrivateKey, localPublicKey, err := BIDECDSA.GenerateKeyPair()
+	localPrivateKey, localPublicKey, err := gohelperfiles.GenerateKeyPair()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Ecdsahelper error: %v\n", err)
 		os.Exit(1)
 	}
-	remotePrivateKey, remotePublicKey, err := BIDECDSA.GenerateKeyPair()
+	remotePrivateKey, remotePublicKey, err := gohelperfiles.GenerateKeyPair()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Ecdsahelper error: %v\n", err)
 		os.Exit(1)
 	}
-	encSharedKey := BIDECDSA.CreateSharedKey(localPrivateKey, remotePublicKey)
-	encryptedText, err := BIDECDSA.EcdsaHelper(BIDECDSA.ENCRYPT, clearText, encSharedKey)
+	encSharedKey := gohelperfiles.CreateSharedKey(localPrivateKey, remotePublicKey)
+	encryptedText, err := gohelperfiles.EcdsaHelper(gohelperfiles.ENCRYPT, clearText, encSharedKey)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Ecdsahelper %s error: %v\n", BIDECDSA.ENCRYPT, err)
+		fmt.Fprintf(os.Stderr, "Ecdsahelper %s error: %v\n", gohelperfiles.ENCRYPT, err)
 		os.Exit(1)
 	}
-	decSharedKey := BIDECDSA.CreateSharedKey(remotePrivateKey, localPublicKey)
-	decryptedText, err := BIDECDSA.EcdsaHelper(BIDECDSA.DECRYPT, encryptedText, decSharedKey)
+	decSharedKey := gohelperfiles.CreateSharedKey(remotePrivateKey, localPublicKey)
+	decryptedText, err := gohelperfiles.EcdsaHelper(gohelperfiles.DECRYPT, encryptedText, decSharedKey)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Ecdsahelper %s error: %v\n", BIDECDSA.DECRYPT, err)
+		fmt.Fprintf(os.Stderr, "Ecdsahelper %s error: %v\n", gohelperfiles.DECRYPT, err)
 		os.Exit(1)
 	}
 	if decryptedText == clearText {
